11e944074aaf18fc69b656de8450bf286a094665
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-02-27 17:37:13 +0530

    Update UI colors in contacts and education components

--- src/components/core-ui/contacts/contacts-ui.js
+++ src/components/core-ui/contacts/contacts-ui.js
@@ -20,7 +20,7 @@ const ContactUI = ({ open, success, errMsg, handleClose, classes, handleContactF
       style={{ backgroundColor: theme.secondary }}
     >
       <div className='contacts--container'>
-        <h1 style={{ color: theme.primary }}>Contacts</h1>
+        <h1 style={{ color: theme.primary }}>Contact Me</h1>
         <div className='contacts-body'>
           <div className='contacts-form'>
             <form ref={form} onSubmit={handleContactForm}>
